640aa398f1cad69600fbfdf0cf5ecad58a445bdb
wuguangnuo <wuguangnuo@qq.com>
2018-07-01 10:59:58 +0800

    管理员 SQL 查询 优化;
    查询->编码->转义->截取

--- Application/Admin/Controller/SqlController.class.php
+++ Application/Admin/Controller/SqlController.class.php
@@ -10,8 +10,9 @@ class SqlController extends AdminController {
 	
 	public function query(){
 		$Form = M();
-		$data['result'] = $Form->execute($_POST['sql']);
-		$data['info'] = cut_str($_POST['sql'], 50, true);
+		$data['execute'] = $Form->execute($_POST['sql']);
+		$data['sql'] = cut_str($_POST['sql'], 50, true);
+		$data['query'] = cut_str(htmlspecialchars(json_encode($Form->query($_POST['sql']))), 3000, true); // 截取，转义，编码，查询
 		$this->ajaxReturn($data); // 默认JSON格式返回
 	}
 }
