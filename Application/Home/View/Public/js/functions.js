4c7bab9f368846919b47a7a7345c0dc9426fcf0a
wuguangnuo <wuguangnuo@qq.com>
2018-07-14 19:26:25 +0800

    HS 价格变化
    328 ->488

--- Application/Home/View/Public/js/functions.js
+++ Application/Home/View/Public/js/functions.js
@@ -11,27 +11,35 @@ $(function(){
 			document.title = '今天，又是充满希望的一天！' + OriginTitile;
 			titleTime = setTimeout(function(){
 				document.title = OriginTitile;
-			},2000);}
+			},2000);
+		}
 	});
+/*
+ * 测试环境 -- 禁用 *
+ * 线上使用加密压缩文件
 
 	//提示复制
 	document.body.oncopy = function(){
 		alert("复制成功！")
 	};
+	
 	//禁止右键
 	document.body.oncontextmenu = function(){
-		return false;
-	};
-	//禁止拖动
-	document.body.ondragstart = function(){
-		return false;
+		return false
 	};
 	
-	//控制台输出
-	eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('k.l("%c%m o n f%c","0:5 6;h-j:g;7:2(\'3://4.d/b/e/8/i.p\') a-9","1-w:\'x y\';1-z:v;1-r:q;0:s","0:5 6;7:2(\'3://4.d/b/e/8/u.t\') a-9");',36,36,'padding|font|url|http|wuguangnuo|47px|50px|background|img|repeat|no|Public||cn|wugn|328|100px|line|alipay|height|console|log|c我|打钱|諾哥|jpg|bold|weight|5px|png|wechat|16px|family|Microsoft|YaHei|size'.split('|'),0,{}));
+	//禁止拖动
+	document.body.ondragstart = function(){
+		return false
+	};
 
+	//控制台输出
+	console.log("%c%c我 諾哥 打钱%c3̶2̶8̶%c488%c","padding:47px 50px;line-height:100px;background:url('http://wuguangnuo.cn/Public/wugn/img/alipay.jpg')no-repeat","font-family:'Microsoft YaHei';font-size:18px;font-weight:bold;padding:5px","font-family:'Microsoft YaHei';font-size:16px;font-weight:normal;padding:5px","font-family:'Microsoft YaHei';font-size:18px;font-weight:bold;color:red;padding:5px","padding:47px 50px;background:url('http://wuguangnuo.cn/Public/wugn/img/wechat.png')no-repeat");
+	
 	//百度统计
 	var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?b058ebe6a38c1861af55616ce957f00e";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s);})();
-
+/*
+ * 结束 *
+ */
 });
 
