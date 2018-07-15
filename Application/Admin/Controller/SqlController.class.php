ea9c1b2742ea8b73d8e08b469ade2fcd0d886c27
wuguangnuo <wuguangnuo@qq.com>
2018-07-15 15:38:00 +0800

    性能优化
    管理->SQL

--- Application/Admin/Controller/SqlController.class.php
+++ Application/Admin/Controller/SqlController.class.php
@@ -11,8 +11,8 @@ class SqlController extends AdminController {
 	public function query(){
 		$Form = M();
 		$data['execute'] = $Form->execute($_POST['sql']);
-		$data['sql'] = cut_str($_POST['sql'], 50, true);
-		$data['query'] = cut_str(htmlspecialchars(json_encode($Form->query($_POST['sql']), 320)), 3000, true); // 截取，转义，编码(JSON_UNESCAPED_UNICODE(256) + JSON_UNESCAPED_SLASHES(64))，查询
+		$data['sql'] = htmlspecialchars(cut_str($_POST['sql'], 50, true));
+		$data['query'] = htmlspecialchars(cut_str(json_encode($Form->query($_POST['sql']), 320), 3000, true)); // 截取，转义，编码(JSON_UNESCAPED_UNICODE(256) + JSON_UNESCAPED_SLASHES(64))，查询
 		$this->ajaxReturn($data); // 默认JSON格式返回
 	}
 }
