036b373f051e92154d72b4e9688763ede7b7d279
wuguangnuo <wuguangnuo@qq.com>
2018-07-15 14:55:28 +0800

    查询优化
    新增标签查询

--- Application/Home/Controller/BlogController.class.php
+++ Application/Home/Controller/BlogController.class.php
@@ -54,21 +54,33 @@ class BlogController extends BlogsideController {
 		$this->display();
 	}
 	
-	public function search($q = 'null') {
+	public function search($q = null, $t = null) {
 		$Blog = M('blog');
-		if($q == 'null'){
-			$where = "1 = 1";
-		}else{
-			$where['post_title'] = array('like', '%'.$q.'%');
-			$where['post_author'] = array('like', '%'.$q.'%');
-			$where['post_type'] = array('like', '%'.$q.'%');
-			$where['post_from'] = array('like', '%'.$q.'%');
-			$where['_logic'] = 'or';
+		if(empty($q) && !empty($t)){
+			//按标签查询
+			$arr = explode(',', $t);
+			foreach($arr as &$val){
+				$val = '%'.$val.'%';
+			}
+			$where['post_type'] = array('like', $arr, 'or');
 		}
+		else{
+			//普通查询
+			if(empty($q)){
+				$where = "1 = 1";
+			}else{
+				$where['post_title'] = array('like', '%'.$q.'%');
+				$where['post_author'] = array('like', '%'.$q.'%');
+				$where['post_type'] = array('like', '%'.$q.'%');
+				$where['post_from'] = array('like', '%'.$q.'%');
+				$where['_logic'] = 'or';
+			}
+		}
+		
 		$count = $Blog->where($where)->count(); // 查询总数
 		$p = getpage($count, 8); // 每页几个
 		$list = $Blog->field(true)->where($where)->order('id desc')->limit($p->firstRow, $p->listRows)->select();
-
+		
 		foreach ($list as $key=>&$val) {
 			$val['post_date'] = cut_str($val['post_date'], 16);
 			$val['post_content'] = str_replace(PHP_EOL, '\r\n', htmlspecialchars(cut_str($val['post_content'], 300, true))); // 简介,转义
