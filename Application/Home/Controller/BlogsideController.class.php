c0ef9e6ab5b1aeba99414ff41b588639af2199f6
wuguangnuo <wuguangnuo@qq.com>
2018-07-18 22:58:22 +0800

    commit;
    字体加宽，字数减少

--- Application/Home/Controller/BlogsideController.class.php
+++ Application/Home/Controller/BlogsideController.class.php
@@ -8,7 +8,7 @@ class BlogsideController extends Controller {
 		$side = $Blog->field('id,post_title')->order('rand()')->limit(8)->select();
 		
 		foreach ($side as $key=>&$val) {
-			$val['post_title'] = cut_str($val['post_title'], 20, true);
+			$val['post_title'] = cut_str($val['post_title'], 17, true);
 		}
 		$this->assign('side', $side); // 赋值数据集
 	}
