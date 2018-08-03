9c8e9bc0b1d41b88c5bbbbe76aeccbbe8cd91a69
wuguangnuo <wuguangnuo@qq.com>
2018-08-04 07:34:32 +0800

    admin登录优化
    admin 登录优化

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,8 +3,8 @@ namespace Admin\Controller;
 use Think\Controller;
 
 class IndexController extends Controller {
-	public function index(){
-		if(isset($_SESSION['username']) && isset($_SESSION['password'])){
+	public function index() {
+		if(isset($_SESSION['username']) && isset($_SESSION['password'])) {
 			$User = M('user');
 			$where['username'] = $_SESSION['username'];
 			$where['password'] = $_SESSION['password'];
@@ -13,42 +13,50 @@ class IndexController extends Controller {
 				$this->assign('username', $_SESSION['username']);
 				$this->assign('meta_title', "管理首页");
 				$this->display();
-			}else{
-				$this->redirect('Index/login','',1,'登录状态失效');
+			} else {
+				$this->redirect('Index/login', '', 1, '登录状态失效');
 			}
-		}
-		else{
-			$this->redirect('Index/login','',1,'未登录');
+		} else {
+			$this->redirect('Index/login', '', 1, '未登录');
 		}
 	}
 	
 	// 登录
-	public function login(){
+	public function login() {
 		session_destroy();
-		if(!empty($_POST['username']) && !empty($_POST['password'])){
+		if(!empty($_POST['username']) && !empty($_POST['password'])) {
 			$User = M('user');
 			$where['username'] = $_POST['username'];
 			$where['password'] = md5($_POST['password']);
 			$data = $User->where($where)->select();
 			if($data) {
-				session_start();
-				$_SESSION['username'] = $_POST['username'];
-				$_SESSION['password'] = md5($_POST['password']);
-				$this->redirect('Index/index','',1,'登录中');
-			}else{
+				if($data[0]['roleid'] == 1) {
+					$User->login_at = date("Y-m-d H-i-s");
+					$result = $User->where($where)->save();
+					if($result) {
+						session_start();
+						$_SESSION['username'] = $_POST['username'];
+						$_SESSION['password'] = md5($_POST['password']);
+						$this->redirect('Index/index', '', 1, '登录中');
+					} else {
+						$this->assign('messages', "更新用户失败");
+					}
+				} else {
+					$this->assign('messages', "权限不足");
+				}
+			} else {
 				$this->assign('messages', "账号或密码错误");
 			}
-		}
-		else{
-			$this->assign('messages', "请输入账号密码");
+		} else {
+			$this->assign('messages', "请输入账号和密码");
 		}
 		$this->assign('meta_title', "登录");
 		$this->display();
 	}
 	
 	// 注销
-	public function logout(){
+	public function logout() {
 		session_destroy();
-		$this->redirect('Index/login','',1,'已注销');
+		$this->redirect('Index/login', '', 1, '已注销');
 	}
 }
