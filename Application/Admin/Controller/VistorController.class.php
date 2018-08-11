4bfe80bda73127156ca4b93f57231e90161d4407
wuguangnuo <wuguangnuo@qq.com>
2018-08-11 09:17:56 +0800

    add vistor view
    add vistor view

--- Application/Admin/Controller/VistorController.class.php
+++ Application/Admin/Controller/VistorController.class.php
@@ -5,21 +5,15 @@ use Think\Controller;
 class VistorController extends AdminController {
 	public function index() {
 		$sql = 'SELECT ip FROM wu_vistor;';
+		$url = "http://ip.taobao.com/service/getIpInfo.php?ip=";
 		$Form = M();
 		$data = json_encode($Form->query($sql), 320);
 		$arr = json_decode($data, true);
-		$url = "http://ip.taobao.com/service/getIpInfo.php?ip=";
-		echo '<table border="1"><thead><th>IP(long)</th><th>IP</th><th>region</th><th>city</th><th>isp</th></thead><tbody>';
-		for($i = 0; $i < count($arr); $i++) {
-			$data = (array)(json_decode(file_get_contents($url.long2ip($arr[$i]['ip']))))->data;
-			echo '<tr>';
-			echo '<td>'.$arr[$i]['ip'].'</td>';
-			echo '<td>'.$data['ip'].'</td>';
-			echo '<td>'.$data['region'].'</td>';
-			echo '<td>'.$data['city'].'</td>';
-			echo '<td>'.$data['isp'].'</td>';
-			echo '</tr>';
+		foreach($arr as $k=>$v) {
+			$vistor[$k] = (array)(json_decode(file_get_contents($url.long2ip($v['ip']))))->data;
 		}
-		echo '</tbody></table>';
+		$this->assign('vistor', $vistor);
+		$this->assign('meta_title', '访客统计');
+		$this->display();
 	}
 }
