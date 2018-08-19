58ea0d0a8357eda4ffd427cc71c4e63db0f68c0b
wuguangnuo <wuguangnuo@qq.com>
2018-08-19 18:05:05 +0800

    模版升级 - 配置部分

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,34 +1,36 @@
 <?php
 return array(
-	// WuGN
-	'AUTHOR_NAME'     => 'WuGN', // 作者
-	'AUTHOR_VERSION'  => '3.0.0', // 版本
-    'AUTHOR_EMAIL'    => 'admin@wuguangnuo.cn', // 邮箱
-    'AUTHOR_QQ'       => '2391459282', // QQ
-	'AUTHOR_PHONE'    => '13646019112', // 电话
-	'WEB_SITE'        => 'wuguangnuo.cn', // 站点
-	'WEB_SITE_DOMAIN' => 'http://www.wuguangnuo.cn', // 域名
-	'WEB_SITE_TITLE'  => '諾wugn', // 标题
-	'WEB_SITE_SLOGAN' => '造轮子ing', // 描述
+    // WuGN
+    'WUGN' => array(
+        'AUTHOR_NAME'     => 'WuGN', // 作者
+        'AUTHOR_VERSION'  => '3.0.3', // 版本
+        'AUTHOR_EMAIL'    => 'admin@wuguangnuo.cn', // 邮箱
+        'AUTHOR_QQ'       => '2391459282', // QQ
+        'AUTHOR_PHONE'    => '13646019112', // 电话
+        'WEB_SITE'        => 'wuguangnuo.cn', // 站点
+        'WEB_SITE_DOMAIN' => 'http://www.wuguangnuo.cn', // 域名
+        'WEB_SITE_TITLE'  => '諾wugn', // 标题
+        'WEB_SITE_SLOGAN' => '造轮子ing', // 描述
+    ),
 
-	// 数据库
-	'DB_TYPE'    => 'mysql', // 数据库类型
-	'DB_HOST'    => '127.0.0.1', // 服务器地址
-	'DB_NAME'    => 'wuguangnuo', // 数据库名
-	'DB_USER'    => 'root', // 用户名
-	'DB_PWD'     => 'root', // 密码
-	'DB_PORT'    => 3306, // 端口
-	'DB_PREFIX'  => 'wu_', // 数据库表前缀
-	'DB_CHARSET' => 'utf8', // 字符集
-	'DB_DEBUG'   => true, // 数据库调试模式 开启后可以记录SQL日志
-	'DB_FIELDS_CACHE'	=>	true, // 开启字段缓存
-	
-	// Trace
-	'SHOW_PAGE_TRACE' => true, // 显示页面Trace信息
+    // 数据库
+    'DB_TYPE'    => 'mysql', // 数据库类型
+    'DB_HOST'    => '127.0.0.1', // 服务器地址
+    'DB_NAME'    => 'wuguangnuo', // 数据库名
+    'DB_USER'    => 'root', // 用户名
+    'DB_PWD'     => 'root', // 密码
+    'DB_PORT'    => 3306, // 端口
+    'DB_PREFIX'  => 'wu_', // 数据库表前缀
+    'DB_CHARSET' => 'utf8', // 字符集
+    'DB_DEBUG'   => true, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_FIELDS_CACHE' => true, // 开启字段缓存
 
-	// 伪静态
-	'URL_HTML_SUFFIX'=>'html|shtml|php|xml', // 伪静态，多个用 | 分割
-	
-	// URL配置
+    // Trace
+    'SHOW_PAGE_TRACE' => true, // 显示页面Trace信息
+
+    // 伪静态
+    'URL_HTML_SUFFIX'=>'html|shtml|php|xml', // 伪静态，多个用 | 分割
+
+    // URL配置
     'URL_CASE_INSENSITIVE' => true, // 不区分大小写
 );
--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -1,19 +1,22 @@
 <?php
 return array(
-	// 路由
-	'URL_ROUTER_ON' => true, //开启路由
-	'URL_ROUTE_RULES' => array( //定义路由规则
-		'/^blog\/(\d+)$/' => 'Blog/read?id=:1',
-	),
-	
-	// 模板相关配置
+    // 路由
+    'URL_ROUTER_ON' => true, //开启路由
+    'URL_ROUTE_RULES' => array( //定义路由规则
+        '/^blog\/(\d+)$/' => 'Blog/read?id=:1',
+    ),
+    
+    // 模板相关配置
     'TMPL_PARSE_STRING' => array(
         '__ROOT__'      => __ROOT__,
         '__PUBLIC__'    => __ROOT__ . '/Public',
-		'__VIEW__'      => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View',
-		'__HOME_CSS__'  => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View/Public/css',
-		'__HOME_JS__'   => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View/Public/js',
-		'__HOME_IMG__'  => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View/Public/img',
+        '__VIEW__'      => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View',
+        '__HOME_CSS__'  => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View/Public/css',
+        '__HOME_JS__'   => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View/Public/js',
+        '__HOME_IMG__'  => __ROOT__ . ltrim(APP_PATH, '.') . 'Home/View/Public/img',
     ),
-	
+    
+    // 模版开始/结束定界符，默认{}
+    'TMPL_L_DELIM' => '{',
+    'TMPL_R_DELIM' => '}',
 );
