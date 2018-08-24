e21d0ebd3901c384307befa81579fa274325a169
wuguangnuo <wuguangnuo@qq.com>
2018-08-24 14:35:44 +0800

    登录验证码

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -24,31 +24,36 @@ class IndexController extends Controller {
 	// 登录
 	public function login() {
 		session_destroy();
-		if(!empty($_POST['username']) && !empty($_POST['password'])) {
-			$User = M('user');
-			$where['username'] = $_POST['username'];
-			$where['password'] = md5($_POST['password']);
-			$data = $User->where($where)->select();
-			if($data) {
-				if($data[0]['roleid'] == 1) {
-					$User->login_at = date("Y-m-d H-i-s");
-					$result = $User->where($where)->save();
-					if($result) {
-						session_start();
-						$_SESSION['username'] = $_POST['username'];
-						$_SESSION['password'] = md5($_POST['password']);
-						$this->redirect('Index/index', '', 1, '登录中');
+		$v = new \Think\Verify();
+		if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['verify'])) {
+			if($v->check($_POST['verify']) === true) {
+				$User = M('user');
+				$where['username'] = $_POST['username'];
+				$where['password'] = md5($_POST['password']);
+				$data = $User->where($where)->select();
+				if($data) {
+					if($data[0]['roleid'] == 1) {
+						$User->login_at = date("Y-m-d H-i-s");
+						$result = $User->where($where)->save();
+						if($result) {
+							session_start();
+							$_SESSION['username'] = $_POST['username'];
+							$_SESSION['password'] = md5($_POST['password']);
+							$this->redirect('Index/index', '', 1, '登录中');
+						} else {
+							$this->assign('messages', "更新用户失败");
+						}
 					} else {
-						$this->assign('messages', "更新用户失败");
+						$this->assign('messages', "权限不足");
 					}
 				} else {
-					$this->assign('messages', "权限不足");
+					$this->assign('messages', "账号或密码错误");
 				}
 			} else {
-				$this->assign('messages', "账号或密码错误");
+				$this->assign('messages', "验证码错误");
 			}
 		} else {
-			$this->assign('messages', "请输入账号和密码");
+			$this->assign('messages', "请填写完整");
 		}
 		$this->assign('meta_title', "登录");
 		$this->display();
@@ -58,5 +63,16 @@ class IndexController extends Controller {
 	public function logout() {
 		session_destroy();
 		$this->redirect('Index/login', '', 1, '已注销');
+	}
+	
+	// 验证码
+	public function verify() {
+		$Verify = new \Think\Verify();
+		$Verify->useImgBg = false;
+		$Verify->fontSize = 20;
+		$Verify->useCurve = false;
+		$Verify->useNoise = false;
+		$Verify->length = 4;
+		$Verify->entry();
 	}
 }
