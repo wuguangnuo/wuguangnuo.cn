fc50e0296a5053771783def5d951980b8e63c830
wuguangnuo <wuguangnuo@qq.com>
2018-08-25 17:15:51 +0800

    Session更新，安全改进

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -3,19 +3,19 @@ namespace Admin\Controller;
 use Think\Controller;
 
 class AdminController extends Controller {
-	protected function _initialize(){
-		if(isset($_SESSION['username']) && isset($_SESSION['password'])){
+	protected function _initialize() {
+		if(session('?username') && session('?password')) {
 			$User = M('user');
-			$where['username'] = $_SESSION['username'];
-			$where['password'] = $_SESSION['password'];
+			$where['username'] = session('username');
+			$where['password'] = session('password');
 			$data = $User->where($where)->select();
 			if($data) {
-				$this->assign('username', $_SESSION['username']);
-			}else{
+				$this->assign('username', session('username'));
+			} else {
 				$this->redirect('Index/login','',1,'登录状态失效');
 			}
 		}
-		else{
+		else {
 			$this->redirect('Index/login','',1,'未登录');
 		}
 	}
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -4,13 +4,13 @@ use Think\Controller;
 
 class IndexController extends Controller {
 	public function index() {
-		if(isset($_SESSION['username']) && isset($_SESSION['password'])) {
+		if(session('?username') && session('?password')) {
 			$User = M('user');
-			$where['username'] = $_SESSION['username'];
-			$where['password'] = $_SESSION['password'];
+			$where['username'] = session('username');
+			$where['password'] = session('password');
 			$data = $User->where($where)->select();
 			if($data) {
-				$this->assign('username', $_SESSION['username']);
+				$this->assign('username', session('username'));
 				$this->assign('meta_title', "管理首页");
 				$this->display();
 			} else {
@@ -23,22 +23,21 @@ class IndexController extends Controller {
 	
 	// 登录
 	public function login() {
-		session_destroy();
-		$v = new \Think\Verify();
-		if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['verify'])) {
+		session(null);
+		if($_POST['username'] && $_POST['password'] && $_POST['verify']) {
+			$v = new \Think\Verify();
 			if($v->check($_POST['verify']) === true) {
 				$User = M('user');
 				$where['username'] = $_POST['username'];
 				$where['password'] = md5($_POST['password']);
 				$data = $User->where($where)->select();
-				if($data) {
+				if(count($data) == 1) {
 					if($data[0]['roleid'] == 1) {
 						$User->login_at = date("Y-m-d H-i-s");
 						$result = $User->where($where)->save();
 						if($result) {
-							session_start();
-							$_SESSION['username'] = $_POST['username'];
-							$_SESSION['password'] = md5($_POST['password']);
+							session('username', $_POST['username']);
+							session('password', md5($_POST['password']));
 							$this->redirect('Index/index', '', 1, '登录中');
 						} else {
 							$this->assign('messages', "更新用户失败");
@@ -61,7 +60,7 @@ class IndexController extends Controller {
 	
 	// 注销
 	public function logout() {
-		session_destroy();
+		session(null);
 		$this->redirect('Index/login', '', 1, '已注销');
 	}
 	
