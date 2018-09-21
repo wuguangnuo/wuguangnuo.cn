be99e2c839b85c34280499fb8f843bec0975f493
wuguangnuo <wuguangnuo@qq.com>
2018-09-21 13:00:41 +0800

    模板 meta 改进

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -11,6 +11,8 @@ return array(
         'WEB_SITE_DOMAIN' => 'http://www.wuguangnuo.cn', // 域名
         'WEB_SITE_TITLE'  => '諾wugn', // 标题
         'WEB_SITE_SLOGAN' => '造轮子ing', // 描述
+        'WEB_DESCRIPTION' => 'My Blog Site Source Code. 这是我的个人网站 wuguangnuo.cn 第三代源代码。基于thinkPHP框架', // 网站描述
+        'WEB_KEYWORDS'    => 'Information Technology, Science, Education, JAVA, Git, HTML, JavaScript, Maven, PHP, Python, SQL', // 网站关键词
     ),
 
     // 数据库
