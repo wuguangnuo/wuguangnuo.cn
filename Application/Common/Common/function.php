723fbb3ba4e7a04cc6e8092145481b8870325e71
wuguangnuo <wuguangnuo@qq.com>
2018-09-25 23:50:46 +0800

    调整分页
    注：涉及到page核心类

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -19,12 +19,12 @@ function cut_str($str, $len, $tail=false){
  */
 function getpage($count, $pagesize = 8) {
 	$p = new Think\Page($count, $pagesize);
-	$p->setConfig('header', '<br /><li class="rows">共<b>%TOTAL_ROW%</b>条记录 第<b>%NOW_PAGE%</b>页/共<b>%TOTAL_PAGE%</b>页</li>');
+	$p->setConfig('header', '<span class="pageRemark">共<strong>%TOTAL_PAGE%</strong>页<strong>%TOTAL_ROW%</strong>条数据</span>');
 	$p->setConfig('prev', '上一页');
 	$p->setConfig('next', '下一页');
-	$p->setConfig('last', '末页');
+	$p->setConfig('last', '尾页');
 	$p->setConfig('first', '首页');
-	$p->setConfig('theme', '%FIRST%%UP_PAGE%%LINK_PAGE%%DOWN_PAGE%%END%%HEADER%');
+	$p->setConfig('theme', '%FIRST%%UP_PAGE%%ELLIPSIS_LEFT%%LINK_PAGE%%ELLIPSIS_RIGHT%%DOWN_PAGE%%END%%HEADER%');
 	$p->lastSuffix = false; // 最后一页不显示为总页数
 	return $p;
 }
