d4b984a39624252c2807144267a034d8cacf56a2
wuguangnuo <wuguangnuo@qq.com>
2018-09-26 09:23:12 +0800

    访客 模型更新

--- Application/Admin/Model/DictionaryModel.class.php
+++ Application/Admin/Model/DictionaryModel.class.php
@@ -5,7 +5,7 @@ use Think\Model;
 class DictionaryModel extends Model {
 	public function getDictionary($group_key) {
 		$where['group_key'] = $group_key;
-		return $this->field(true)->where($where)->select();
+		return $this->field('code_index,code_note,group_concat(code_value)as code_value')->where($where)->group('code_index')->order('id asc')->select();
 	}
 }
 ?>
