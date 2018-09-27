d848f3cca0ed4f90cbce441b7a87152d40641ed3
wuguangnuo <wuguangnuo@qq.com>
2018-09-27 14:16:44 +0800

    博客搜索优化
    全面使用 AJAX　加载

--- Application/Home/Controller/BlogController.class.php
+++ Application/Home/Controller/BlogController.class.php
@@ -4,8 +4,32 @@ use Think\Controller;
 
 class BlogController extends BlogsideController {
     public function index() {
+		$this->assign('meta_title', '諾的博客');
+		$this->display();
+    }
+	
+	public function loadTable($q = null, $t = null) {
 		$Blog = M('blog');
-		$where = '1 = 1';
+		if(empty($q) && !empty($t)) {
+			//按标签查询
+			$arr = explode(',', $t);
+			foreach($arr as &$val) {
+				$val = '%'.$val.'%';
+			}
+			$where['post_type'] = array('like', $arr, 'or');
+		} else {
+			//普通查询
+			if(empty($q)) {
+				$where = '1 = 1';
+			} else {
+				$where['post_title'] = array('like', '%'.$q.'%');
+				$where['post_author'] = array('like', '%'.$q.'%');
+				$where['post_type'] = array('like', '%'.$q.'%');
+				$where['post_from'] = array('like', '%'.$q.'%');
+				$where['_logic'] = 'or';
+			}
+		}
+		
 		$count = $Blog->where($where)->count(); // 查询总数
 		$p = getpage($count, 8); // 每页几个
 		$list = $Blog->field(true)->where($where)->order('id desc')->limit($p->firstRow, $p->listRows)->select();
@@ -14,12 +38,12 @@ class BlogController extends BlogsideController {
 			$val['post_date'] = cut_str($val['post_date'], 16);
 			$val['post_content'] = str_replace("\n", '\r\n', str_replace(PHP_EOL, '\r\n', htmlspecialchars(cut_str($val['post_content'], 300, true)))); // 简介,转义
 		}
-		
-		$this->assign('meta_title', '諾的博客');
-		$this->assign('list', $list);
-		$this->assign('page', $p->show());
-		$this->display();
-    }
+		$data['keyword'] = strtoupper($q ? '文章搜索：'.$q : ($t ? '文章分类：'.$t : '最近文章'));
+		$data['list'] = $list;
+		$data['pages'] = $p->show();
+
+		$this->ajaxReturn($data);
+	}
 	
 	public function read($id = 1) {
 		$Blog = M('blog');
@@ -52,44 +76,5 @@ class BlogController extends BlogsideController {
 		$this->lp = $lp;
 		$this->np = $np;
 		$this->display();
-	}
-	
-	public function search($q = null, $t = null) {
-		$Blog = M('blog');
-		if(empty($q) && !empty($t)) {
-			//按标签查询
-			$arr = explode(',', $t);
-			foreach($arr as &$val) {
-				$val = '%'.$val.'%';
-			}
-			$where['post_type'] = array('like', $arr, 'or');
-		} else {
-			//普通查询
-			if(empty($q)) {
-				$where = '1 = 1';
-			} else {
-				$where['post_title'] = array('like', '%'.$q.'%');
-				$where['post_author'] = array('like', '%'.$q.'%');
-				$where['post_type'] = array('like', '%'.$q.'%');
-				$where['post_from'] = array('like', '%'.$q.'%');
-				$where['_logic'] = 'or';
-			}
-		}
-		
-		$count = $Blog->where($where)->count(); // 查询总数
-		$p = getpage($count, 8); // 每页几个
-		$list = $Blog->field(true)->where($where)->order('id desc')->limit($p->firstRow, $p->listRows)->select();
-		
-		foreach ($list as &$val) {
-			$val['post_date'] = cut_str($val['post_date'], 16);
-			$val['post_content'] = str_replace(PHP_EOL, '\r\n', htmlspecialchars(cut_str($val['post_content'], 300, true))); // 简介,转义
-		}
-		$keyword = strtoupper($q?'文章搜索：'.$q:'文章分类：'.$t);
-		
-		$this->assign('list', $list);
-		$this->assign('keyword', $keyword);
-		$this->assign('page', $p->show()); // 赋值分页输出
-		$this->assign('meta_title', '搜索博客');
-		$this->display(); // 模版输出
 	}
 }
