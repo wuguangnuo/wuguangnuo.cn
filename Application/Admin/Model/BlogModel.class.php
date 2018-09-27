bb938a6c9ca7e1f1afb97a590a8079f76c08bb2a
wuguangnuo <wuguangnuo@qq.com>
2018-09-27 14:31:27 +0800

    错误修复

--- Application/Admin/Model/BlogModel.class.php
+++ Application/Admin/Model/BlogModel.class.php
@@ -5,8 +5,7 @@ use Think\Model;
 class BlogModel extends Model {
 	
 	public function getLast() {
-		$data = $this->field(true)->order('id desc')->limit(1)->select();
-		return $data[0];
+		return $this->field(true)->order('id desc')->limit(1)->find();
 	}
 }
 ?>
