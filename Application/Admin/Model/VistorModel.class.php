25ce40d373488f590656b6d0fdb4cb126ce570bf
wuguangnuo <wuguangnuo@qq.com>
2018-10-20 17:20:10 +0800

    模型 计数函数独立出来

--- Application/Admin/Model/VistorModel.class.php
+++ Application/Admin/Model/VistorModel.class.php
@@ -60,127 +60,15 @@ class VistorModel extends Model {
 	public function getSystemCount($systemShow){
 		$where['ag'] = array('notlike', '%bot%');
 		$agent = $this->field('ag')->where($where)->select();
-		foreach($agent as $v){
-			$v = $v['ag'];
-			if (preg_match('/sitemap/i', $v) || preg_match('/Parser/i', $v)) {
-				$data['sitemap'] += 1;
-			} else if (preg_match('/spider/i', $v)){
-				$data['spider'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/[^.\d]95/i', $v)) {
-				$data['Windows 95'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/[^.\d]98/i', $v)) {
-				$data['Windows 98'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*5.0/i', $v)) {
-				$data['Windows 2000'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*5.1/i', $v)) {
-				$data['Windows XP'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*5.2/i', $v)) {
-				$data['Windows XP'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*6.0/i', $v)) {
-				$data['Windows Vista'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*6.1/i', $v)) {
-				$data['Windows 7'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*6.2/i', $v)) {
-				$data['Windows 8'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt\s*10.0/i', $v)) {
-				$data['Windows 10'] += 1;
-			} else if (preg_match('/win/i', $v) && preg_match('/nt/i', $v)) {
-				$data['Windows NT'] += 1;
-			} else if (preg_match('/android\s*([\d\.]+)/i', $v, $Android)) {
-				$data['Android'] += 1;
-			} else if (preg_match('/iphone/i', $v)) {
-				$data['iPhone'] += 1;
-			} else if (preg_match('/ipad/i', $v)) {
-				$data['iPad'] += 1;
-			} else if (preg_match('/linux/i', $v)) {
-				$data['Linux'] += 1;
-			} else if (preg_match('/unix/i', $v)) {
-				$data['Unix'] += 1;
-			} else if (preg_match('/sun/i', $v) && preg_match('/os/i', $v)) {
-				$data['SunOS'] += 1;
-			} else if (preg_match('/ibm/i', $v) && preg_match('/os/i', $v)) {
-				$data['IBM OS/2'] += 1;
-			} else if (preg_match('/Mac/i', $v) && preg_match('/PC/i', $v)) {
-				$data['Macintosh'] += 1;
-			} else if (preg_match('/Macintosh/i', $v) && preg_match('/Mac/i', $v)) {
-				$data['MacOS'] += 1;
-			} else if (preg_match('/PowerPC/i', $v)) {
-				$data['PowerPC'] += 1;
-			} else if (preg_match('/AIX/i', $v)) {
-				$data['AIX'] += 1;
-			} else if (preg_match('/HPUX/i', $v)) {
-				$data['HPUX'] += 1;
-			} else if (preg_match('/NetBSD/i', $v)) {
-				$data['NetBSD'] += 1;
-			} else if (preg_match('/BSD/i', $v)) {
-				$data['BSD'] += 1;
-			} else if (preg_match('/OSF1/i', $v)) {
-				$data['OSF1'] += 1;
-			} else if (preg_match('/IRIX/i', $v)) {
-				$data['IRIX'] += 1;
-			} else if (preg_match('/FreeBSD/i', $v)) {
-				$data['FreeBSD'] += 1;
-			} else if (preg_match('/teleport/i', $v)) {
-				$data['teleport'] += 1;
-			} else if (preg_match('/flashget/i', $v)) {
-				$data['flashget'] += 1;
-			} else if (preg_match('/webzip/i', $v)) {
-				$data['webzip'] += 1;
-			} else if (preg_match('/offline/i', $v)) {
-				$data['offline'] += 1;
-			} else {
-				$others += 1;
-			}
-		}
-		arsort($data);//根据值降序排序
-		foreach($data as $k=>&$v){
-			if(!in_array($k, $systemShow)){
-				$others += $v;
-				unset($data[$k]);
-			}
-		}
-		$data['others'] = $others;
-		return($data);
+		$data = get_os_count($agent, $systemShow);
+		return $data;
 	}
 	
 	public function getBrowserCount($browserShow){
 		$where['ag'] = array('notlike', '%bot%');
 		$agent = $this->field('ag')->where($where)->select();
-		foreach($agent as $v){
-			$v = $v['ag'];
-			if (preg_match('/sitemap/i', $v) || preg_match('/Parser/i', $v)) {
-				$data['sitemap'] += 1;
-			} else if (preg_match('/spider/i', $v) || preg_match('/bot/i', $v)) {
-				$data['spider'] += 1;
-			} else if (stripos($v, 'Firefox/')) {
-				$data['Firefox'] += 1;
-			} else if (stripos($v, 'Maxthon')) {
-				$data['Maxthon'] += 1;
-			} else if (stripos($v, 'MSIE')) {
-				$data['IE'] += 1;
-			} else if (stripos($v, 'OPR')) {
-				$data['Opera'] += 1;
-			} else if (stripos($v, 'Edge')) {
-				$data['Edge'] += 1;
-			} else if (stripos($v, 'Chrome')) {
-				$data['Chrome'] += 1;
-			} else if (stripos($v, 'Safari')) {
-				$data['Safari'] += 1;
-			} else if (stripos($v, 'rv:') && stripos($v, 'Gecko')) {
-				$data['IE'] += 1;
-			} else {
-				$others += 1;
-			}
-		}
-		arsort($data);//根据值降序排序
-		foreach($data as $k=>&$v){
-			if(!in_array($k, $browserShow)){
-				$others += $v;
-				unset($data[$k]);
-			}
-		}
-		$data['others'] = $others;
-		return($data);
+		$data = get_br_count($agent, $browserShow);
+		return $data;
 	}
 }
 ?>
