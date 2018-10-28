9dec050041cb036033aea9e370e64c67a4e3f34d
wuguangnuo <wuguangnuo@qq.com>
2018-10-28 23:14:53 +0800

    public img 对象存储

--- Application/Home/View/Public/js/functions.js
+++ Application/Home/View/Public/js/functions.js
@@ -34,7 +34,7 @@ $(function(){
 	};
 
 	//控制台输出
-	console.log("%c%c我 諾哥 打钱%c3̶2̶8̶%c488%c","padding:49px 50px;line-height:100px;background:url('http://wuguangnuo.cn/Public/wugn/img/alipay.jpg')no-repeat","font-family:'Microsoft YaHei';font-size:18px;font-weight:bold;padding:5px","font-family:'Microsoft YaHei';font-size:16px;font-weight:normal;padding:5px","font-family:'Microsoft YaHei';font-size:18px;font-weight:bold;color:red;padding:5px","padding:49px 50px;background:url('http://wuguangnuo.cn/Public/wugn/img/wechat.png')no-repeat");
+	console.log("%c%c我 諾哥 打钱%c3̶2̶8̶%c488%c","padding:49px 50px;line-height:100px;background:url('https://wuguangnuo-1257896087.cos.ap-guangzhou.myqcloud.com/public/img/alipay.jpg')no-repeat","font-family:'Microsoft YaHei';font-size:18px;font-weight:bold;padding:5px","font-family:'Microsoft YaHei';font-size:16px;font-weight:normal;padding:5px","font-family:'Microsoft YaHei';font-size:18px;font-weight:bold;color:red;padding:5px","padding:49px 50px;background:url('https://wuguangnuo-1257896087.cos.ap-guangzhou.myqcloud.com/public/img/wechat.png')no-repeat");
 
 	//百度统计
 	var _hmt=_hmt||[];(function(){var hm=document.createElement("script");hm.src="https://hm.baidu.com/hm.js?b058ebe6a38c1861af55616ce957f00e";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(hm,s);})();
