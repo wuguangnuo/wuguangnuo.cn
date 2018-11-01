2913b02b5beb3d5ef38be34a025ee96c9a25389f
wuguangnuo <wuguangnuo@qq.com>
2018-11-01 16:21:56 +0800

    隐藏标签

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,6 +1,7 @@
 <?php
 //名字过长截断
 function cut_str($str, $len, $tail=false){
+	$str = strip_tags($str);
 	if(mb_strlen($str, 'utf-8') > $len){
 		$str = mb_substr($str, 0, $len, 'utf-8');
 		if($tail){
