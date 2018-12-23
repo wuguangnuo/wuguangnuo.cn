ea3d3e24f758eb33e69a4d08215e7a8a2995f5da
吴广诺 <wuguangnuo@qq.com>
2018-12-23 22:22:57 +0800

    Blog Edit
    You can edit blog here, but bug for html code..(issues)

--- Application/Admin/Controller/BlogController.class.php
+++ Application/Admin/Controller/BlogController.class.php
@@ -23,19 +23,54 @@ class BlogController extends AdminController {
 		$result = $Form->add();
 		if($result) {
 			$this->ajaxReturn("1");
-		}else{
+		} else {
 			$this->ajaxReturn("save error!");
 		}
 	}
 	
-	public function read(){
+	public function read($id = null){
 		$Form = D('blog');
-		$data = $Form->getLast();
+		if($id) {
+			$data = $Form->getBlogById($id);
+		} else {
+			$data = $Form->getLast();
+		}
+		
 		if($data) {
 			$this->data = $data;
-		}else{
+		} else {
 			$this->error('没找到');
 		}
 		$this->display();
+	}
+
+	public function edit($id = null){
+		$Form = D('blog');
+		if($id) {
+			$data = $Form->getBlogById($id);
+		} else {
+			$data = $Form->getLast();
+		}
+		
+		if($data) {
+			$this->data = $data;
+		} else {
+			$this->error('没找到');
+		}
+		$this->assign('meta_title', "编辑Blog");
+		$this->display();
+	}
+
+	public function update(){
+		$Form = M('blog');
+		$data = I('post.');
+		$data['post_content'] = I('post.post_content', 'get post error!', '');
+		$result = $Form->save($data);
+		
+		if($result) {
+			$this->ajaxReturn("1");
+		} else {
+			$this->ajaxReturn("save error!");
+		}
 	}
 }
