964dba7c3f1e8be4dcd27767a57b031b0e555d13
吴广诺 <wuguangnuo@qq.com>
2020-04-03 10:44:18 +0800

    fix but for ip2long
    i thing this is a scripting language bug with php.

--- Application/Home/Controller/VistorController.class.php
+++ Application/Home/Controller/VistorController.class.php
@@ -4,10 +4,16 @@ use Think\Controller;
 
 class VistorController extends Controller {
 	protected function _initialize() {
+		// 修复IP过大问题，适应数据库int(4)
+		$ip = get_client_ip();
+		if($ip > 128*256*256*256){
+			$ip -= 256*256*256*256;
+		}
+		
 		$Vistor = M('vistor');
 		$Vistor->create();
 		$Vistor->lk = $_SERVER['PHP_SELF'];
-		$Vistor->ip = ip2long(get_client_ip());
+		$Vistor->ip = ip2long($ip);
 		$Vistor->ag = $_SERVER['HTTP_USER_AGENT'];
 		$Vistor->tm = date('Y-m-d H:i:s');
 		$Vistor->add();
